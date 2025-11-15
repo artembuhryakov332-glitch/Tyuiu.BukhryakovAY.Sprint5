2e6170ee85aad9080bd486ea48f4f84a1a9a31e2
Артем Бухряков <artembuhryakov332@gmail.com>
2025-11-15 21:34:50 +0500

    Task1

---
+++ Tyuiu.BukhryakovAY.Sprint5.Task1.V13.Lib/DataService.cs
@@ -0,0 +1,25 @@
+﻿using tyuiu.cources.programming.interfaces;
+using tyuiu.cources.programming.interfaces.Sprint5;
+using System.IO;
+namespace Tyuiu.BukhryakovAY.Sprint5.Task1.V13.Lib
+{
+    public class DataService : ISprint5Task1V13
+    {
+        public string SaveToFileTextData(int startValue, int stopValue)
+        {
+            string path = Path.Combine(Path.GetTempPath(),"OutPutFileTask1.txt");
+            double funcValue = 0;
+            for(int i = startValue; i < stopValue; i++)
+            {
+                funcValue = ((2 * i - 3) / (Math.Cos(i) + i)) + 5;
+                if (double.IsInfinity(funcValue))
+                {
+                   funcValue = 0;
+                }
+             string result = funcValue.ToString();
+             File.AppendAllText(path, $"{result}\n");
+            }
+            return path;
+        }
+    }
+}
---
+++ Tyuiu.BukhryakovAY.Sprint5.Task1.V13/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.IO;
+using Tyuiu.BukhryakovAY.Sprint5.Task1.V13.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                       **");
+        Console.WriteLine("***************************************************************************");
+        int start = -5;
+        Console.WriteLine("страрт диапозон функции:" + start);
+        int end = 5;
+        Console.WriteLine("конец диапозон функции:" + end);
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Результат:                                                             **");
+        Console.WriteLine("***************************************************************************");
+        string result = ds.SaveToFileTextData(-5, 5);
+        Console.WriteLine("Файл: " + result);
+
+        double funcValue = 0;
+        for (int i = start; i < end + 1; i++)
+        {
+            funcValue = ((2 * i - 3) / (Math.Cos(i) + i)) + 5;
+            if (double.IsInfinity(funcValue))
+            {
+                funcValue = 0;
+            }
+            string yValue = funcValue.ToString();
+            Console.WriteLine($"  f({i,2}) = {funcValue,10:F4}");
+        }
+        Console.ReadKey();
+    }
+}
