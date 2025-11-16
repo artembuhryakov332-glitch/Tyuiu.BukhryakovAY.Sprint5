e47fbb47d64e6f639449624baadde63990e8c731
Артем Бухряков <artembuhryakov332@gmail.com>
2025-11-16 15:21:54 +0500

    Task2

--- Tyuiu.BukhryakovAY.Sprint5.Task2.V3.Lib/DataService.cs
+++ Tyuiu.BukhryakovAY.Sprint5.Task2.V3.Lib/DataService.cs
@@ -9,6 +9,7 @@ namespace Tyuiu.BukhryakovAY.Sprint5.Task2.V3.Lib
             string path = Path.Combine(Path.GetTempPath(), "OutPutFileTask2.txt");
             for(int i = 0 ; i < matrix.GetLength(0); i++)
             {
+                int[] row = new int[matrix.GetLength(1)];
                 for (int j = 0; j < matrix.GetLength(1); j++)
                 {
                     if (matrix[i,j] % 2 != 0)
@@ -16,9 +17,9 @@ namespace Tyuiu.BukhryakovAY.Sprint5.Task2.V3.Lib
                         matrix[i,j] = 0;
                     }
                     Console.Write(matrix[i, j] + ";");
-                    File.AppendAllText(path, matrix[i,j].ToString() + ";");
+                    row[j] = matrix[i, j];
                 }
-                File.AppendAllText(path,"\n");
+                File.AppendAllText(path, string.Join(";", row) + "\n");
                 Console.WriteLine();
             }
             return path;
